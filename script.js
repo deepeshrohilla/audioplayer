b9022b3c94d08f9107d8baf02db7dbcdda8bd4b8
deepeshrohilla <50067114+deepeshrohilla@users.noreply.github.com>
2020-08-25 12:41:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-$(document).ready(
+$(document).ready(function(){
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').trigger('focus')
     })
-)
+})
+// document.getElementById("#toggleItem").style.display = ""
 
 var audio = document.getElementById("audio")
 var addAudioFiles = document.getElementById("addFile")
 var rotateImage = document.getElementById("audioImg")
 var volume = document.getElementById("volume")
+// my default playlist
+var defaultPlaylist = [
+    "Alan Walker - Fade [NCS Release].mp3",
+    "Alan Walker, Sabrina Carpenter & Farruko - On My Way.mp3",
+    "Dharia - (Uu Nai Na) Sugar And Brownies.mp3",
+    "Faded - Alan Walker.mp3"
+]
+
 var i = 0
 var files = null
 
@@ -17,7 +26,7 @@ volume.addEventListener('change', () => {
     audio.volume = volume.value / 100
 })
 
-// Add files dynamically from user
+// Add audio files dynamically from user
 addAudioFiles.onchange = (e) => {
     this.files = e.target.files
     console.log(this.files)
@@ -36,10 +45,22 @@ addAudioFiles.onchange = (e) => {
     }
 }
 
+// default playlist button
+var dfPlaylistBtn = document.getElementById("dfPlaylistBtn")
+dfPlaylistBtn.addEventListener("click",()=>{
+    audio.src = "audio/" + this.defaultPlaylist[0]
+    var songTitle = document.getElementById("songTitle")
+    songTitle.innerText = `Song Title = ${this.defaultPlaylist[0]}`    
+})
+
 // play button
 function play() {
     audio.play()
-    this.rotateImage.style.animationDuration = "5s"
+    if(audio.src == ""){
+        this.rotateImage.style.animationDuration = "0s"
+    }else{
+        this.rotateImage.style.animationDuration = "5s"
+    }
 }
 // pause button
 function pause() {
@@ -49,27 +70,55 @@ function pause() {
 
 // next button 
 function next() {
-    if (i < files.length - 1) {
-        i++
+    if (files == null) {
+        if (i < defaultPlaylist.length - 1) {
+            i++
+        } else {
+            i = 0
+        }
+        console.log(i)
+        audio.src = "audio/" + defaultPlaylist[i]
+        audio.play()
+        this.rotateImage.style.animationDuration = "5s"
+        songTitle.innerText = `Song Title = ${this.defaultPlaylist[i]}`
     } else {
-        i = 0
-    }
-    console.log(i)
-    audio.src = "audio/" + files.item(i).name
-    audio.play()
-    songTitle.innerText = `Song Title = ${this.files.item(i).name}`
+        if (i < files.length - 1) {
+            i++
+        } else {
+            i = 0
+        }
+        console.log(i)
+        audio.src = "audio/" + files.item(i).name
+        audio.play()
+        this.rotateImage.style.animationDuration = "5s"
+        songTitle.innerText = `Song Title = ${this.files.item(i).name}`
+    } 
 }
 // previous button
 function prev() {
-    if (i > 0) {
-        i--
+    if (files == null) {
+        if (i > 0) {
+            i--
+        } else {
+            i = defaultPlaylist.length - 1
+        }
+        console.log(i)
+        audio.src = "audio/" + defaultPlaylist[i]
+        audio.play()
+        this.rotateImage.style.animationDuration = "5s"
+        songTitle.innerText = `Song Title = ${this.defaultPlaylist[i]}`
     } else {
-        i = files.length - 1
+        if (i > 0) {
+            i--
+        } else {
+            i = files.length - 1
+        }
+        console.log(i)
+        audio.src = "audio/" + files.item(i).name
+        audio.play()
+        this.rotateImage.style.animationDuration = "5s"
+        songTitle.innerText = `Song Title = ${this.files.item(i).name}`
     }
-    console.log(i)
-    audio.src = "audio/" + files.item(i).name
-    audio.play()
-    songTitle.innerText = `Song Title = ${this.files.item(i).name}`
 }
 
 // change audio track position
